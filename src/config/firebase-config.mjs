d9ca2c124b2d5f5efcf94df268e1f5bddba00202
Nicolas Sabogal Torres <nicolas.sabogalto@utadeo.edu.co>
2023-11-09 19:56:23 -0500

    Update firebase-config.mjs solution 2

--- src/config/firebase-config.mjs
+++ src/config/firebase-config.mjs
@@ -2,21 +2,23 @@
 import dotenv from "dotenv";
 import { initializeApp, applicationDefault  } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import { getStorage, ref } from "firebase-admin/storage";
+import { getStorage } from "firebase-admin/storage";
 
 dotenv.config();
 
 const storage = getStorage();
-const reference =  await ref(storage, "pills_and_caps.png")
 
 initializeApp({
     credential: admin.credential.cert({
       "project_id": process.env.FIREBASE_PROJECT_ID,
       "private_key": process.env.FIREBASE_PRIVATE_KEY,
       "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+       "storageBucket": process.env.FIREBASE_PROJECT_ID+".appspot.com"
     }),
     databaseURL: "https://my-firebase-app.firebaseio.com"
   })
+
+const reference = storage.bucket();
 
 const db = getFirestore();
 

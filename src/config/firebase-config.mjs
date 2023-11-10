04b04920c82efdc51f86e6b7e804a123a5c73edc
Nicolas Sabogal Torres <nicolas.sabogalto@utadeo.edu.co>
2023-11-09 20:27:14 -0500

    Update firebase-config.mjs solved 3

--- src/config/firebase-config.mjs
+++ src/config/firebase-config.mjs
@@ -1,20 +1,21 @@
 // init firebase
 import dotenv from "dotenv";
-import { initializeApp, applicationDefault  } from "firebase-admin/app";
+import { initializeApp, cert  } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
 dotenv.config();
 
-initializeApp({
-    credential: admin.credential.cert({
-      "project_id": process.env.FIREBASE_PROJECT_ID,
-      "private_key": process.env.FIREBASE_PRIVATE_KEY,
-      "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-       "storageBucket": process.env.FIREBASE_PROJECT_ID+".appspot.com"
-    }),
-    databaseURL: "https://my-firebase-app.firebaseio.com"
-  })
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+}
+
+initializeApp(config)
 
 
 const storage = getStorage();
